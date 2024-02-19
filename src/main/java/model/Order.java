127d2726fe80b34c2b149b4b94c3eea5f5279d53
Ankush Desai <ankush@Ankushs-MacBook-Air.local>
2024-02-18 20:39:03 -0500

    added javadoc comments for model Order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,6 +1,10 @@
 package model;
 
-
+/**
+ * 
+ * Class for Order of the players
+ *
+ */
 public class Order {
 
     private String d_Order;
@@ -15,14 +19,29 @@ public class Order {
         deploy();
     }
 
+    /**
+     * This method sends the result after execution of execute method
+     * 
+     * @return returns if the order is successfully executed or not.
+     */
     public String getExecuteResult() {
         return this.d_ExecuteResult;
     }
 
+    /**
+     * This method is used to set the result after the execution of the execute
+     * method.
+     * 
+     * @param p_ExecuteResult String to set the executed result
+     */
     public void setExecuteResult(String p_ExecuteResult) {
         this.d_ExecuteResult = p_ExecuteResult;
     }
 
+    /**
+     * Method to check if the command issued is correct or not and set country name
+     * and the armies
+     */
     public void deploy() {
         String[] l_Splitted = d_Order.split(" ");
         if (l_Splitted[0].equals("deploy")) {
@@ -33,10 +52,18 @@ public class Order {
         }
     }
 
+    /**
+     * This method returns the order of the player
+     * 
+     * @return the order issues by the player
+     */
     public String getOrder() {
         return this.d_Order;
     }
 
+    /**
+     * This method is used to execute the orders issued by the players.
+     */
     public void execute() {
         int l_Flag = 0;
         for (Country l_Country : d_GameModel.getSelectedMap().getCountryList()) {
