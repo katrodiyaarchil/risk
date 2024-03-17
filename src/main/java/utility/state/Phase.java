a9637612baa5aa68bb8fcea948bc059a41031aab
mihirgediya2001 <mihirgediya2001@gmail.com>
2024-03-17 16:10:08 -0400

    adding methods to phase class

--- src/main/java/utility/state/Phase.java
+++ src/main/java/utility/state/Phase.java
@@ -1,5 +1,37 @@
 package utility.state;
 
+import controller.GameEngine;
+import view.CommandPrompt;
+
 public abstract class Phase {
 
+    public GameEngine d_Ge;
+    public CommandPrompt d_Vw;
+
+    public Phase(GameEngine p_Ge, CommandPrompt p_Vw) {
+        d_Ge = p_Ge;
+        d_Vw = p_Vw;
+    }
+
+    abstract public String editMap(String p_S);
+
+    abstract public String editCountry(String p_S, String p_S1);
+
+    abstract public String editContinent(String p_S, String p_S1);
+
+    abstract public String editNeighbor(String p_S, String p_S1);
+
+    abstract public String saveMap(String p_S);
+
+    abstract public String validateMap();
+
+    abstract public String loadMap(String p_S);
+
+    abstract public String addPlayers(String p_S, String p_S1);
+
+    abstract public void assignCountries();
+
+    abstract public void showMap();
+
+    abstract public String getPhaseName();
 }
