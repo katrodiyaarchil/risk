3dda9752714e4f113af190a69bacab67495726b0
Krupalidobariya <dobariyakrupali208@gmail.com>
2024-03-17 17:12:02 -0400

    implementing ExecuteOrder.java

---
+++ src/main/java/utility/state/ExecuteOrder.java
@@ -0,0 +1,110 @@
+package utility.state;
+
+import controller.GameEngine;
+import observerpattern.LogEntryBuffer;
+import view.CommandPrompt;
+
+
+public class ExecuteOrder extends Phase {
+    LogEntryBuffer d_Leb;
+
+
+    public ExecuteOrder(GameEngine p_Ge, CommandPrompt p_Vw) {
+        super(p_Ge, p_Vw);
+        try {
+            d_Leb = new LogEntryBuffer();
+            d_Leb.setResult("This is the Execute Order Phase");
+            d_Ge.getPlayerController().playerNextOrder();
+
+            if (!d_Ge.getPhase().getPhaseName().equals("GameOver")) {
+                d_Ge.showMap(this);
+                d_Ge.setPhase(new Reinforcement(d_Ge, d_Vw));
+
+            }
+
+        } catch (Exception p_E) {
+        }
+    }
+
+
+    @Override
+    public String editMap(String p_S) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+    @Override
+    public String editCountry(String p_S, String p_S1) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String editContinent(String p_S, String p_S1) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String editNeighbor(String p_S, String p_S1) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String saveMap(String p_S) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String loadMap(String p_S) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String addPlayers(String p_S, String p_S1) {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public void assignCountries() {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+    }
+
+    @Override
+    public void showMap() {
+        d_Ge.showMap(this);
+    }
+
+
+    @Override
+    public String validateMap() {
+        d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
+        d_Leb.setResult("Invalid command in state ");
+        return null;
+    }
+
+
+    @Override
+    public String getPhaseName() {
+        return "ExecutePhase";
+    }
+
+}
