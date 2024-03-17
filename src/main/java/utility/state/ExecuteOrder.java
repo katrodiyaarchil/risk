e862a5fe547dd27ecb8283f1202ee98d45b4c759
Krupalidobariya <dobariyakrupali208@gmail.com>
2024-03-17 17:14:15 -0400

    update ExecuteOrder.java

--- src/main/java/utility/state/ExecuteOrder.java
+++ src/main/java/utility/state/ExecuteOrder.java
@@ -3,11 +3,20 @@ package utility.state;
 import controller.GameEngine;
 import observerpattern.LogEntryBuffer;
 import view.CommandPrompt;
-
+/**
+ * The ExecuteOrder phase extends the Phase class and implements methods specific to this phase.
+ * It handles executing orders during gameplay.
+ */
 
 public class ExecuteOrder extends Phase {
     LogEntryBuffer d_Leb;
-
+    /**
+     * Constructs an ExecuteOrder object with a GameEngine and CommandPrompt object, initializing the log entry buffer
+     * and proceeding to execute orders.
+     *
+     * @param gameEngine The GameEngine object
+     * @param commandPrompt The CommandPrompt object
+     */
 
     public ExecuteOrder(GameEngine p_Ge, CommandPrompt p_Vw) {
         super(p_Ge, p_Vw);
@@ -26,14 +35,18 @@ public class ExecuteOrder extends Phase {
         }
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String editMap(String p_S) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String editCountry(String p_S, String p_S1) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -41,14 +54,18 @@ public class ExecuteOrder extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String editContinent(String p_S, String p_S1) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String editNeighbor(String p_S, String p_S1) {
@@ -57,7 +74,9 @@ public class ExecuteOrder extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String saveMap(String p_S) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -65,7 +84,9 @@ public class ExecuteOrder extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String loadMap(String p_S) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -73,7 +94,9 @@ public class ExecuteOrder extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String addPlayers(String p_S, String p_S1) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -81,26 +104,34 @@ public class ExecuteOrder extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void assignCountries() {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showMap() {
         d_Ge.showMap(this);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String validateMap() {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String getPhaseName() {
