c6f0d3996c642121bb95776add2c15323cc2a88a
Krupalidobariya <dobariyakrupali208@gmail.com>
2024-03-17 17:35:41 -0400

    update GameOver.java

--- src/main/java/utility/state/GameOver.java
+++ src/main/java/utility/state/GameOver.java
@@ -3,12 +3,21 @@ package utility.state;
 import controller.GameEngine;
 import observerpattern.LogEntryBuffer;
 import view.CommandPrompt;
-
+/**
+ * The GameOver Phase extends the Phase class and implements methods specific to this phase.
+ * It handles the end of the game and provides relevant acknowledgements.
+ */
 
 public class GameOver extends Phase {
     LogEntryBuffer d_Leb;
 
-
+    /**
+     * Constructs a GameOver object with a GameEngine and CommandPrompt object, initializing the log entry buffer
+     * and ending the game with relevant acknowledgements.
+     *
+     * @param p_Ge The GameEngine object
+     * @param p_Vw The CommandPrompt object
+     */
     public GameOver(GameEngine p_Ge, CommandPrompt p_Vw) {
         super(p_Ge, p_Vw);
         d_Vw.clearTextArea();
@@ -17,14 +26,18 @@ public class GameOver extends Phase {
         d_Leb.setResult("This is Game over phase");
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String editMap(String p_S) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String editCountry(String p_S, String p_S1) {
@@ -32,7 +45,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String editContinent(String p_S, String p_S1) {
@@ -40,7 +55,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String editNeighbor(String p_S, String p_S1) {
@@ -48,7 +65,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String saveMap(String p_S) {
@@ -56,7 +75,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public String validateMap() {
@@ -64,7 +85,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
         return null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String loadMap(String p_S) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -72,7 +95,9 @@ public class GameOver extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String addPlayers(String p_S, String p_S1) {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
@@ -80,13 +105,17 @@ public class GameOver extends Phase {
         return null;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void assignCountries() {
         d_Vw.setCommandAcknowledgement("Invalid command in state " + this.getClass().getSimpleName() + "\n");
         d_Leb.setResult("Invalid command in state ");
     }
-
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void showMap() {
@@ -94,7 +123,9 @@ public class GameOver extends Phase {
         d_Leb.setResult("Invalid command in state ");
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getPhaseName() {
         // TODO Auto-generated method stub
