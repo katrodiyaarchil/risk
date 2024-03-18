85b17effa1b5a3794ea594d1e22a4f8076e7ca18
Ankush4101 <ankushdesai4101@gmail.com>
2024-03-17 21:06:35 -0400

    added javadoc for LogEntrybuffer-observerpattern

--- src/main/java/observerpattern/LogEntryBuffer.java
+++ src/main/java/observerpattern/LogEntryBuffer.java
@@ -1,20 +1,37 @@
 package observerpattern;
 
+/**
+ * Class which extends the Observable class. To log any changes in file, we use
+ * the object of this class.
+ */
 public class LogEntryBuffer extends Observable{
     private String d_result;
     private Logger d_logger;
 
-
+    /**
+     * Constructor which creates the object of Observer logger and attach it with
+     * this class.
+     */
     public LogEntryBuffer(){
         d_logger = new Logger();
         this.attach(d_logger);
     }
 
+    /**
+     * This method is called when we want to log anything in the file.
+     * 
+     * @param p_Result String to be logged.
+     */
     public void setResult(String p_result){
         this.d_result = p_result;
         notify(this);
     }
 
+    /**
+     * This method returns the string which was logged.
+     * 
+     * @return Result string which was logged
+     */
     public String getResult(){
         return this.d_result;
     }
