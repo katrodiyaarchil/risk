729d6025d18d7ad37dd0b275e3eae0ebc72b7c4f
Ankush4101 <ankushdesai4101@gmail.com>
2024-03-17 21:11:40 -0400

    added javadoc for observable

--- src/main/java/observerpattern/Observable.java
+++ src/main/java/observerpattern/Observable.java
@@ -1,18 +1,40 @@
 package observerpattern;
 
 import java.util.ArrayList;
-
+/**
+ * Observable class which has methods to connect/disconnect with observers and
+ * notifies if there is any update.
+ */
 public class Observable {
 
     ArrayList<Observer> d_observers = new ArrayList<Observer>();
-
+    
+	/**
+	 * This method attaches the Observer object with the observable.
+	 * 
+	 * @param p_Observer Object of Observer which is going to be attached with
+	 *                   Observable.
+	 */
     public void attach(Observer p_observer){
         this.d_observers.add(p_observer);
     }
+
+    /**
+	 * This method detaches the Observer object with the observable.
+	 * 
+	 * @param p_Observer Object of Observer which is going to be detached with
+	 *                   Observable.
+	 */
     public void detach(Observer p_observer){
         this.d_observers.remove(p_observer);
     }
-
+    
+	/**
+	 * This is notify method of the Observable which calls the update method on
+	 * every change/addition of log.
+	 * 
+	 * @param p_Observable Object of observable
+	 */
     public void notify(Observable p_observable){
         for(Observer l_observer : d_observers){
             l_observer.update(p_observable);
